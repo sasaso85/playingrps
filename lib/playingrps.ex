8548d5e50ca96cb14dce78a82d93bdfeadaf8615
Saul Sanchez <sasaso85@MacBook-Pro-de-Saul.local>
2023-02-21 20:07:55 -0600

    Initial project

---
+++ lib/playingrps.ex
@@ -0,0 +1,18 @@
+defmodule Playingrps do
+  @moduledoc """
+  Documentation for `Playingrps`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Playingrps.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/playingrps_test.exs
@@ -0,0 +1,8 @@
+defmodule PlayingrpsTest do
+  use ExUnit.Case
+  doctest Playingrps
+
+  test "greets the world" do
+    assert Playingrps.hello() == :world
+  end
+end
