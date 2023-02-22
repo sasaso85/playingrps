37f59e6d54ad8bdff918e0500fbba24bcc3d35d9
Saul Sanchez S <sasaso85@gmail.com>
2023-02-22 00:17:59 -0600

    Add outputs to show inforrmation about selection and guard in some clauses

--- lib/rock_paper_scissors.ex
+++ lib/rock_paper_scissors.ex
@@ -3,17 +3,19 @@ defmodule RockPaperScissors do
     valid_choices = ["rock", "paper", "scissors"]
     computer_choice = Enum.random(valid_choices)
     
+    IO.puts "player choice: #{player_choice} computer_choice: #{computer_choice}"
+IO.puts "Result: "
     if player_choice in valid_choices do
       case {player_choice, computer_choice} do
-        {"rock", "scissors"}, {"paper", "rock"}, {"scissors", "paper"} ->
+        value when value in [{"rock", "scissors"}, {"paper", "rock"}, {"scissors", "paper"}] ->
           IO.puts "You win!"
-        {"rock", "paper"}, {"paper", "scissors"}, {"scissors", "rock"} ->
+        value when value in [{"rock", "paper"}, {"paper", "scissors"}, {"scissors", "rock"}] ->
           IO.puts "Computer wins!"
         _ ->
           IO.puts "It's a tie!"
       end
     else
-      IO.puts "Invalid choice. Please choose from #{valid_choices}."
+      IO.puts "Invalid choice. Please choose from #{Enum.join(valid_choices, ", ")}."
     end
   end
 end
